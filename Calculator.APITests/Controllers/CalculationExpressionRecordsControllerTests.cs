c2dc63b2b38247e21bfac2201ad5f2506d5d237a
DannyJPN <krupadan@email.cz>
2024-07-16 12:21:51 +0200

    New tests added,makign Calculation.UnitTest obsolete. This will be removed.

---
+++ Calculator.APITests/Controllers/CalculationExpressionRecordsControllerTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Calculator.API.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.API.Controllers.Tests
+{
+    [TestClass()]
+    public class CalculationExpressionRecordsControllerTests
+    {
+        [TestMethod()]
+        public void CalculationExpressionRecordsControllerTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void SaveCalculationRecordTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CalculateTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void LoadLastCalculationsTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ Calculator.MathLibTests/MathLibTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Calculator.MathLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.MathLib.Tests
+{
+    [TestClass()]
+    public class MathLibTests
+    {
+        [TestMethod()]
+        public void CalculateTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
